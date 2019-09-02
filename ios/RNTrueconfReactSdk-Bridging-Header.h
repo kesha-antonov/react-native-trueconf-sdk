9e09df532c36f0fc29510b0c8796f865f3106890
Kesha Antonov <innokenty.longway@gmail.com>
2019-09-02 14:44:58 +0300

    add briding header

---
+++ ios/RNTrueconfReactSdk-Bridging-Header.h
@@ -0,0 +1,13 @@
+#ifndef RNTrueconfReactSdk_Bridging_Header_h
+#define RNTrueconfReactSdk_Bridging_Header_h
+
+#import "React/RCTBridgeModule.h"
+
+#import "React/RCTViewManager.h"
+#import <React/RCTBridgeModule.h>
+#import "React/RCTEventEmitter.h"
+#import "React/RCTUIManager.h"
+
+#import "TrueConfSDK/TrueConfSDK.h"
+
+#endif
